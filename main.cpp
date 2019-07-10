a2f0899f26ffbe071b959e9241b9a5292609c329
wsobczyk <sobczyk-wojciech@wp.pl>
2019-07-10 13:55:21 +0200

    Przepisanie przykladu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class logger_engine {
+    ofstream ofs;
+public:
+    logger_engine() {
+        ofs.open("log.txt", ofstream::out | ofstream::app);
+    }
+    ~logger_engine() {
+        ofs.close();
+    }
+    void log (const string& line) {
+        ofs << line << endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    logger_engine l;
+    l.log("some line");
     return 0;
 }
